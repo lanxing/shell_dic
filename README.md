34cf4ef4d2c999594e561b81c5677ac172b6f604
eric.gf <eric.gf@alibaba-inc.com>
2017-07-06 16:14:13 +0800

    增加shell脚本

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 ## shell命令行下词典，支持中英文互相查询
 
 #### 底层通过有道开放平台调用有道词典查询接口，并缓存至本地MySql，再次查询时不需要重新调用接口
+
+--
+#### 依赖包
+##### peewee：Python下轻量级orm框架
++ 文档地址:http://docs.peewee-orm.com/en/latest/
+
+##### requests：http请求包
++ 文档地址:http://docs.python-requests.org/en/master/
+
+--
+#### 有道API开放平台:http://ai.youdao.com/docs/api.s
+
+--
+使用方法
--- src/dic.py
+++ src/dic.py
@@ -1,9 +1,13 @@
+#! /usr/bin/env python3.5
+
 import hashlib
 import random
 import requests
+import re
 from peewee import *
 
 db = MySQLDatabase('mydb', user='root')
+zhPattern = re.compile(u'[\u4e00-\u9fa5]+')
 
 
 # 基础类
@@ -22,6 +26,18 @@ def getMd5(src):
     md5 = hashlib.md5()
     md5.update(src.encode(encoding='utf8'))
     return md5.hexdigest()
+
+
+def containChinese(contents):
+    match = zhPattern.search(contents)
+    if match:
+        return True
+    return False
+
+
+def createTable(table):
+    db.connect()
+    db.create_table(table)
 
 
 def showInfo(json):
@@ -49,34 +65,47 @@ def showInfo(json):
             print('-------------')
 
 
-appKey = '3785101610c308d5'
-salt = random.randint(32768, 65536)
-key = 'bPn9FXAVZLqvmlz1kAXFCUwK2yW7Tw1m'
+def executeDic():
+    appKey = '3785101610c308d5'
+    salt = random.randint(32768, 65536)
+    key = 'bPn9FXAVZLqvmlz1kAXFCUwK2yW7Tw1m'
 
-params = {
-    'q': None,
-    'from': 'auto',
-    'to': 'auto',
-    'appKey': appKey,
-    'salt': salt,
-    'sign': None
-}
-remindStr = '请输入需要查询的单词或语句(q for quit):'
-word = input(remindStr)
-db.connect()
-while word is not None and word != 'q':
-    try:
-        value = MyDic.get(MyDic.key == word).value
-        showInfo(eval(value))
-    except DoesNotExist:
-        params['q'] = word
-        md5Src = appKey + word + str(salt) + key
-        params['sign'] = getMd5(md5Src)
-        r = requests.get('http://openapi.youdao.com/api', params=params)
-        value = r.json()
-        if value.get('errorCode') == '0':
-            MyDic.create(key=word, value=value)
-            showInfo(value)
-        else:
-            print(value)
+    params = {
+        'q': None,
+        'from': 'auto',
+        'to': 'auto',
+        'appKey': appKey,
+        'salt': salt,
+        'sign': None
+    }
+    remindStr = '请输入需要查询的单词或语句(q for quit):'
     word = input(remindStr)
+    db.connect()
+
+    # 如果是中文的话则只缓存字符串长度小于5的翻译结果
+    wordLenLimit = 5
+    while word is not None and word != 'q':
+        try:
+            value = MyDic.get(MyDic.key == word).value
+            showInfo(eval(value))
+        except DoesNotExist:
+            params['q'] = word
+            md5Src = appKey + word + str(salt) + key
+            params['sign'] = getMd5(md5Src)
+            r = requests.get('http://openapi.youdao.com/api', params=params)
+            value = r.json()
+            if value.get('errorCode') == '0':
+
+                if containChinese(word) is False or len(word) < wordLenLimit:
+                    MyDic.create(key=word, value=value)
+                showInfo(value)
+            else:
+                print(value)
+        word = input(remindStr)
+
+
+def main():
+    executeDic()
+
+if __name__ == '__main__':
+    main()
